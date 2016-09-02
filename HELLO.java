1ac49582c5a9f3092e9ed860245d2a30593e1ccb
JAYAPRAKASHJPR <jayaprakashssakthivel@gmail.com>
2016-09-02 03:52:24 -0700

    Add files via upload

---
+++ HELLO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author jp
+ */
+public class HELLO 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) 
+    {
+        int i=0;
+        while(i<5)
+        {
+        
+            System.out.println("hello world");
+            i++;
+        }
+    }
+    
+}
