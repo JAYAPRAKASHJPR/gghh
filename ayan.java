d5eb8e099b4d961a0aedb133e8394c207337137d
JAYAPRAKASHJPR <jayaprakashssakthivel@gmail.com>
2016-09-06 00:40:02 -0700

    Create ayan.java

---
+++ ayan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ayan
+{
+public static void main(String ar[])throws Exception
+{
+int t;
+Scanner sc=new Scanner(System.in);
+t=sc.nextInt();
+System.out.println("Enter the roll numnber\n");
+for(int i=1;i<=100;i++)
+{
+if(t>=i&&t<=i)
+{
+t=sc.nextInt();
+}
+}
+System.out.println("valid input:"+t);
+}
+}
