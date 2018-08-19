7cea83f845476c4258280d7545ed4e865a3d7f53
JAYAPRAKASHJPR <jayaprakashssakthivel@gmail.com>
2018-08-19 17:26:41 +0530

    Add files via upload

---
+++ Print_3rd_Char.java
@@ -0,0 +1,14 @@
+
+public class Print_3rd_Char {
+	public static void main(String args[]) {
+		
+		String s = "String";
+		 char c = s.charAt(0);
+		 char th = s.charAt(2);
+		 
+	   System.out.println(c + "" + th );
+		
+		
+	}
+
+}
